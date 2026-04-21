7295f08dd11d4b07358da949a76fcd0b94c3c323
Richardrlp91 <richardrlp91@gmail.com>
2026-04-21 22:52:21 +0200

    Creación de modo light y modo dark

--- functions.php
+++ functions.php
@@ -11,3 +11,31 @@ function ricardo_leal_enqueue_styles() {
     // Carga el estilo de este tema hijo
     wp_enqueue_style( 'child-style', get_stylesheet_uri(), array( 'parent-style' ), '1.0.0' );
 }
+
+// Script para el Toggle de Modo Oscuro/Claro
+add_action('wp_footer', function() {
+    ?>
+    <script>
+        const toggleBtn = document.getElementById('theme-toggle');
+        const body = document.body;
+        const currentTheme = localStorage.getItem('theme');
+
+        // Verificar preferencia guardada
+        if (currentTheme === 'light') {
+            body.classList.add('light-mode');
+        }
+
+        if (toggleBtn) {
+            toggleBtn.addEventListener('click', () => {
+                body.classList.toggle('light-mode');
+                
+                let theme = 'dark';
+                if (body.classList.contains('light-mode')) {
+                    theme = 'light';
+                }
+                localStorage.setItem('theme', theme);
+            });
+        }
+    </script>
+    <?php
+});
